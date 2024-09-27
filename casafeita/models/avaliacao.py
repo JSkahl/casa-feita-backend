425b2f81e81de283f924e960262dcf3c3d1bf5bb
JSkahl <josekahl333@gmail.com>
2024-09-27 13:06:40 -0300

    chore: Estruturando o básico da model Avaliação

--- casafeita/models/avaliacao.py
+++ casafeita/models/avaliacao.py
@@ -5,6 +5,10 @@ class Avaliacao(models.Model):
     descricao = models.CharField(max_length=255, blank=True, null=True)
     rating = models.DecimalField(decimal_places=1, max_digits=2, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0)
     data = models.DateField(auto_now_add=True)
-
+        
     def __str__(self):
         return f"Avaliação: {self.rating} | Postado: {self.data}"
+
+    class Meta:
+        verbose_name = "Avaliação"
+        verbose_name_plural= "Avaliações"
