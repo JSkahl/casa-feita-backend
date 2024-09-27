5393e0138cb66d52768e064935695995bd2a2a77
JSkahl <josekahl333@gmail.com>
2024-09-27 09:16:59 -0300

    chore: Terminando ajustes na model avaliacao

--- casafeita/models/avaliacao.py
+++ casafeita/models/avaliacao.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Avaliacao(models.Model):
-    conteudo = models.CharField(max_length=255, blank=True, null=True)
-    ranking = models.CharField(max_length=1)
-    data = models.DateField()
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+    rating = models.DecimalField(decimal_places=1, max_digits=2, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0)
+    data = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.ranking
+        return f"Avaliação: {self.rating} | Postado: {self.data}"
