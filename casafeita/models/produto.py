fe5c2e8986a281632861104ce9ae637f3227a82d
JSkahl <josekahl333@gmail.com>
2024-09-30 19:16:23 -0300

    feat: Adicionando campos de imagens em algumas models

--- casafeita/models/produto.py
+++ casafeita/models/produto.py
@@ -3,6 +3,7 @@ from django.db import models
 from .categoria import Categoria
 from .cor import Cor
 from .fabricante import Fabricante
+from uploader.models import Image
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
@@ -12,6 +13,7 @@ class Produto(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="categorias")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="cores")
     fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    foto = models.ForeignKey(Image, related_name="+", on_delete=models.SET_NULL, blank=True, null=True, default=None)
 
     def __str__(self):
         return f"{self.nome} ({self.cor}) | R${self.preco} | {self.quantidade} em estoque."
