e51e95dbd1b72f89e965a57e65657924a27495bf
JSkahl <josekahl333@gmail.com>
2024-09-30 18:46:58 -0300

    chore: Ajustando model Compra

--- casafeita/models/compra.py
+++ casafeita/models/compra.py
@@ -1,27 +1,42 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 from .produto import Produto
 
 class Endereco(models.Model):
     rua = models.CharField(max_length=150)
-    numero = models.IntegerField(max_length=10)
     complemento = models.CharField(max_length=150)
-    cep = models.IntegerField(max_length=20)
+    numero = models.IntegerField(validators=[MaxValueValidator(999)])
+    cep = models.IntegerField(validators=[MaxValueValidator(99999999)])
 
-class Cartao(models.Model):
-    numero = models.IntegerField(max_length=16)
-    nome = models.CharField(max_length=150)
-    validade = models.DateField()
-    cvv = models.IntegerField(max_length=3)
+    class Meta:
+        verbose_name = "Endereço"
+        verbose_name_plural = "Endereços"
 
 class Compra(models.Model):
-    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT)
-    cartao = models.ForeignKey(Cartao, on_delete=models.PROTECT)
+    class StatusCompra(models.IntegerChoices):
+        CARRINHO = 1, "Carrinho"
+        CONCLUIDA = 2, "Concluida"
+        PREPARACAO = 3, "Preparação"
+        SAIDA = 4, "Saída"
+        ENTREGUE = 5, "Entregue"
 
-    def __str__(self):
-        return f"{self.item.produto.nome} | {self.item.quantidade} unidades."
+    class MetodoPagamento(models.IntegerChoices):
+        CARTAO_CREDITO = 1, "Cartão de crédito"
+        CARTAO_DEBITO = 2, "Cartão de débito"
+        PIX = 3, "Pix"
+        BOLETO = 4, "Boleto"
+
+    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+    metodo_pagamento = models.IntegerField(choices=MetodoPagamento.choices)
+    data = models.DateField(auto_now_add=True)
 
 class ItemCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="compras")
     produto = models.ForeignKey(Produto, on_delete=models.PROTECT, related_name="produtos")
-    quantidade = models.IntegerField(max_length=10)
+    quantidade = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Item Compra"
+        verbose_name_plural = "Itens da Compra"
