67dcf89569d88f99a6172c524fdd61ffdd583ae5
JSkahl <josekahl333@gmail.com>
2024-10-01 05:42:53 -0300

    fix: ajustes em alguns serializers em relação ao campo de imagem

--- casafeita/models/avaliacao.py
+++ casafeita/models/avaliacao.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+from uploader.models import Image
+
 class Avaliacao(models.Model):
     descricao = models.CharField(max_length=255, blank=True, null=True)
     rating = models.DecimalField(decimal_places=1, max_digits=2, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0)
     data = models.DateField(auto_now_add=True)
+    foto = models.ForeignKey(Image, related_name="+", on_delete=models.SET_NULL, blank=True, null=True, default=None)
         
     def __str__(self):
         return f"Avaliação: {self.rating} | Postado: {self.data}"
